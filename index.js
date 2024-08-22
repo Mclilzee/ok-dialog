330ca20e82f09a458cbd85b39683ffb311a6e955
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-22 14:44:17 +0200

    Add display message

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+const xButton = document.querySelector("#x");
+const okButton = document.querySelector(".ok-button");
+const message = document.querySelector(".message");
+
+xButton.addEventListener("mouseenter", () => {
+  if (xButton.classList.contains("left-x")) {
+    xButton.textContent = "OK";
+  } else {
+    xButton.classList.add("left-x");
+  }
+});
+
+xButton.addEventListener("mouseleave", () => {
+  xButton.textContent = "X";
+});
+
+
+xButton.addEventListener("click", displayThankYouMessage);
+
+okButton.addEventListener("click", displayThankYouMessage);
+
+function displayThankYouMessage() {
+  message.textContent = "THANK YOU FOR YOUR LIFE, HOME, AND CAR";
+  xButton.styles.display = none;
+  okButton.styles.display = none;
+}
